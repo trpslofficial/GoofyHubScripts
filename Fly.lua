b31f03b236ab36df87411d8da9898ea219025bf0
trpslofficial <114937550+trpslofficial@users.noreply.github.com>
2022-10-16 20:02:36 +0200

    Update Fly.lua

--- Fly.lua
+++ Fly.lua
@@ -1,181 +1,97 @@
-local function NQWSTGE_fake_script()
-    local script = Instance.new('LocalScript', Frame)
- 
-    local plr = script.Parent.Parent.Parent.Parent
-    repeat wait() until plr and plr.Character and plr.Character:findFirstChild("HumanoidRootPart") and plr.Character:findFirstChild("Humanoid") 
-    local mouse = game.Players.LocalPlayer:GetMouse()  
-    repeat wait() until mouse
-    
-    local torso = plr.Character.HumanoidRootPart 
-    local flying = false
-    local deb = true 
-    local ctrl = {f = 0, b = 0, l = 0, r = 0} 
-    local lastctrl = {f = 0, b = 0, l = 0, r = 0} 
-    local maxspeed = 1000 
-    local speed = 50
-    function Fly() 
-    local bg = Instance.new("BodyGyro", torso) 
-    bg.P = 9e4 
-    bg.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
-    bg.cframe = torso.CFrame 
-    local bv = Instance.new("BodyVelocity", torso) 
-    bv.velocity = Vector3.new(0,0.1,0) 
-    bv.maxForce = Vector3.new(9e9, 9e9, 9e9) 
-    repeat wait() 
-    plr.Character.Humanoid.PlatformStand = true 
-    if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then 
-    speed = speed+.5+(speed/maxspeed) 
-    if speed > maxspeed then 
-    speed = maxspeed 
-    end 
-    elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then 
-    speed = speed-1 
-    if speed < 0 then 
-                    speed = 0
-                else
-                    speed = 50
-    end 
-    end 
-    if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
-    bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
-    lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r} 
-    elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then 
-    bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
-    else 
-    bv.velocity = Vector3.new(0,0.1,0) 
-    end 
-    bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0) 
-    until not flying 
-    ctrl = {f = 0, b = 0, l = 0, r = 0} 
-    lastctrl = {f = 0, b = 0, l = 0, r = 0} 
-        
-    bg:Destroy() 
-    bv:Destroy() 
-        plr.Character.Humanoid.PlatformStand = false 
-        speed = 50
-    end 
-    
-    mouse.KeyDown:connect(function(key) 
-    if key:lower() == "e" then 
-            if flying then flying = false 
-                speed = 50
-    else 
-    flying = true 
-    Fly()
-    
-    end 
-    elseif key:lower() == "w" then 
-    ctrl.f = 1 
-    elseif key:lower() == "s" then 
-    ctrl.b = -1 
-    elseif key:lower() == "a" then 
-    ctrl.l = -1 
-    elseif key:lower() == "d" then 
-    ctrl.r = 1 
-    end 
-    end) 
-    mouse.KeyUp:connect(function(key) 
-    if key:lower() == "w" then 
-    ctrl.f = 0 
-    elseif key:lower() == "s" then 
-    ctrl.b = 0 
-    elseif key:lower() == "a" then 
-    ctrl.l = 0 
-    elseif key:lower() == "d" then 
-    ctrl.r = 0 
-    end 
-    end)
-    
-    plr.Character.Humanoid.StateChanged:Connect(function(o,n)
-        if n == Enum.HumanoidStateType.Running then
-            ctrl.f = 1
-        else
-            ctrl.f = 0
+--Waits until the player is in game
+repeat wait()
+until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Torso") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid")
+local mouse = game.Players.LocalPlayer:GetMouse()
+
+--Waits until the player's mouse is found
+repeat wait() until mouse
+
+--Variables
+local plr = game.Players.LocalPlayer
+local torso = plr.Character.Torso
+local flying = true
+local deb = true
+local ctrl = {f = 0, b = 0, l = 0, r = 0}
+local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+local maxspeed = 50
+local speed = 0
+local bg = nil
+local bv = nil
+
+--Actual flying
+function Fly()
+	game.StarterGui:SetCore("SendNotification", {Title="Fly Activated"; Text="WeAreDevs.net"; Duration=1;})
+    bg = Instance.new("BodyGyro", torso)
+    bg.P = 9e4
+    bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+    bg.cframe = torso.CFrame
+    bv = Instance.new("BodyVelocity", torso)
+    bv.velocity = Vector3.new(0,0.1,0)
+    bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
+    repeat wait()
+      plr.Character.Humanoid.PlatformStand = true
+      if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
+        speed = speed+.5+(speed/maxspeed)
+        if speed > maxspeed then
+          speed = maxspeed
         end
-    
-    end)
-    script.Parent.TextButton.MouseButton1Click:Connect(function()
-        if flying then
-            flying = false
-            speed = 50
-        else
-            flying = true
-            Fly()
+      elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
+        speed = speed-1
+        if speed < 0 then
+          speed = 0
         end
-    end)
-    
+      end
+      if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+        lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
+      elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+      else
+        bv.velocity = Vector3.new(0,0.1,0)
+      end
+      bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0)
+    until not flying
+    ctrl = {f = 0, b = 0, l = 0, r = 0}
+    lastctrl = {f = 0, b = 0, l = 0, r = 0}
+    speed = 0
+    bg:Destroy()
+	bg = nil
+    bv:Destroy()
+	bv = nil
+    plr.Character.Humanoid.PlatformStand = false
+	game.StarterGui:SetCore("SendNotification", {Title="Fly Deactivated"; Text="WeAreDevs.net"; Duration=1;})
 end
-coroutine.wrap(NQWSTGE_fake_script)()
-local function RAQA_fake_script() -- Frame.Buttons 
-    local script = Instance.new('LocalScript', Frame)
- 
-    local Trigger = script.Parent.MiniTrext
-    local IsMini = false
-    function CreateTween(Instance,Style,Direction,Time,table,RepeatCount,CanRepeat,Delay)
-        local ts = game:GetService("TweenService")
-        local TweenInfo = TweenInfo.new(Time,Style,Direction,RepeatCount,CanRepeat,Delay)
-        local Tween = ts:Create(Instance,TweenInfo,table)
-        repeat wait() until Tween ~= nil
-        return Tween
-        
-    end
-    Trigger.MouseButton1Click:Connect(function()
-        if IsMini then
-            CreateTween(script.Parent,Enum.EasingStyle.Quad,Enum.EasingDirection.Out,0.5,{Size = UDim2.new(0.265, 0,0.1, 0)},0,false,0.1):Play()
-            IsMini = false
-            Trigger.Text = "-"
-        else
-            CreateTween(script.Parent,Enum.EasingStyle.Quad,Enum.EasingDirection.Out,0.5,{Size = UDim2.new(0.265, 0,0.042, 0)},0,false,0.1):Play()
-            IsMini = true
-            Trigger.Text = "+"
-        end
-    end)
-    script.Parent.Delete.MouseButton1Click:Connect(function()
-        script.Parent.Parent:Destroy()
-    end)
-end
-coroutine.wrap(RAQA_fake_script)()
-local function TKVUMP_fake_script() -- Frame.Drag Gui 
-    local script = Instance.new('LocalScript', Frame)
- 
-    local UserInputService = game:GetService("UserInputService")
-    
-    local gui = script.Parent
-    
-    local dragging
-    local dragInput
-    local dragStart
-    local startPos
-    
-    local function update(input)
-        local delta = input.Position - dragStart
-        gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-    end
-    
-    gui.InputBegan:Connect(function(input)
-        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-            dragging = true
-            dragStart = input.Position
-            startPos = gui.Position
-            
-            input.Changed:Connect(function()
-                if input.UserInputState == Enum.UserInputState.End then
-                    dragging = false
-                end
-            end)
-        end
-    end)
-    
-    gui.InputChanged:Connect(function(input)
-        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-            dragInput = input
-        end
-    end)
-    
-    UserInputService.InputChanged:Connect(function(input)
-        if input == dragInput and dragging then
-            update(input)
-        end
-    end)
-end
-coroutine.wrap(TKVUMP_fake_script)()
+
+--Controls
+mouse.KeyDown:connect(function(key)
+	if key:lower() == "e" then
+		if flying then 
+			flying = false
+		else
+			flying = true
+			Fly()
+		end
+	elseif key:lower() == "w" then
+		ctrl.f = 1
+	elseif key:lower() == "s" then
+		ctrl.b = -1
+	elseif key:lower() == "a" then
+		ctrl.l = -1
+	elseif key:lower() == "d" then
+		ctrl.r = 1
+	end
+end)
+
+mouse.KeyUp:connect(function(key)
+	if key:lower() == "w" then
+		ctrl.f = 0
+	elseif key:lower() == "s" then
+		ctrl.b = 0
+	elseif key:lower() == "a" then
+		ctrl.l = 0
+	elseif key:lower() == "d" then
+		ctrl.r = 0
+	end
+end)
+
+Fly()
